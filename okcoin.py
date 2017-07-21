38141c86c6e0f2ce25ee464684bea3035c6845f6
migege <lzw.whu@gmail.com>
2017-07-21 17:47:52 +0800

    update

--- okcoin.py
+++ okcoin.py
@@ -11,7 +11,28 @@ sys.setdefaultencoding('utf8')
 
 
 def run():
-    try:
+    symbols = [
+        {
+            'name': 'BTC/CNY',
+            'symbol': 0,
+            'key_price': 'btcLast',
+        },
+        {
+            'name': 'ETH/CNY',
+            'symbol': 2,
+            'key_price': 'ethLast',
+        },
+        {
+            'name': 'LTC/CNY',
+            'symbol': 1,
+            'key_price': 'ltcLast',
+        },
+    ]
+
+    link = 'https://www.okcoin.cn'
+
+    fb = Feedback()
+    for symbol in symbols:
         url = 'https://www.okcoin.cn/real/ticker.do?random=%s' % int(time.time() * 1000)
         headers = {
             'Host': 'www.okcoin.cn',
@@ -19,26 +40,35 @@ def run():
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.89 Safari/537.36',
         }
         data = {
-            'symbol': 1,
+            'symbol': symbol['symbol'],
         }
-        link = 'https://www.okcoin.cn'
 
-        r = requests.post(url, headers=headers, data=data)
-        res = r.json()
+        try:
+            r = requests.post(url, headers=headers, data=data)
+            res = r.json()
 
-        rows = []
-        rows.append({'name': 'BTC/CNY', 'price': res['btcLast']})
-        rows.append({'name': 'ETH/CNY', 'price': res['ethLast']})
-        rows.append({'name': 'LTC/CNY', 'price': res['ltcLast']})
+            kwargs = {
+                'name': symbol['name'],
+                'price': res[symbol['key_price']],
+                'low': res['low'],
+                'high': res['high'],
+                'change': res['change'],
+                'coinBalance': res['coinBalance'],
+                'money': res['money'],
+                'changeSign': '+' if float(res['change']) > 0 else '-',
+            }
 
-        fb = Feedback()
-        for row in rows:
-            kwargs = {'title': '{name}: {price}'.format(**row), 'subtitle': '', 'arg': link}
-            fb.addItem(**kwargs)
-        fb.output()
-    except Exception, e:
-        print 'EXCEPT:', e
-        pass
+            item = {
+                'title': '{name}: {price} ({changeSign}{change}%)'.format(**kwargs),
+                'subtitle': 'Low: {low}, High: {high}'.format(**kwargs),
+                'arg': link,
+            }
+            fb.addItem(**item)
+        except Exception, e:
+            print 'EXCEPT:', e
+            continue
+
+    fb.output()
 
 
 if __name__ == '__main__':
